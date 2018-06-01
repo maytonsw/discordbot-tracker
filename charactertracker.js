113689c8ea47cec27b5e4aa1424debe1d1d92136
maytonsw <38571572+maytonsw@users.noreply.github.com>
2018-06-01 02:00:17 -0400

    added multi-user functionality & regional support

--- charactertracker.js
+++ charactertracker.js
@@ -7,7 +7,8 @@ const bot = new Discord.Client({ // Load bot
     autorun: true
 });
 
-var tarServer, tarRegion, tarCharacter, logsKey, tarMetric, pctSum, pctRank, avgRank, logs, returnString;
+var tarServer, tarRegion, tarCharacter, tarMetric, pctSum, pctRank, avgRank, returnString;
+const logsKey = "879f290422d74824299a945f46eefca3";
 
 const stdin = process.stdin;         // Use the terminal to run JS code
 stdin.on("data", function(input) {
@@ -28,10 +29,10 @@ bot.on("message", function(user, userID, channelID, message, event) { // Message
     if (message.startsWith(prefix)) { // Message starts with prefix
         let command = message.slice(prefix.length).split(" "); // Split message into words
         switch (command[0]) { // Execute code depending on first word
-        case "ping": // ping: reply "pong"
+        case "ping": // ping: reply "pong"                                  // Template Function. Kept for testing.
             bot.sendMessage({to: channelID, message: "Pong!"});
             break;
-        case "roll": // roll: choose a random number
+        case "roll": // roll: choose a random number                        // Template Function. Kept for testing.
             let max = parseInt(command[1]) || 100;
             let min = 1;
             let result = Math.floor(Math.random()*(max-min+1)+min);
@@ -39,15 +40,10 @@ bot.on("message", function(user, userID, channelID, message, event) { // Message
             console.log("From "+min+" to "+max+", you rolled: **"+result+"**");
             break;
         case "tracknick":
-            
-            tarServer = "sargeras";
-            tarRegion = "US";
-            tarCharacter = "kfcbaer";
-            logsKey = "879f290422d74824299a945f46eefca3";
-            tarMetric = "dps";
-            var apiURL = "https://www.warcraftlogs.com:443/v1/rankings/character/" + tarCharacter + "/" + tarServer + "/" + tarRegion + "?metric=" + tarMetric + "&api_key=" + logsKey;
-            pctSum = 0;
-            pctRank = 0;
+            var apiURL = "https://www.warcraftlogs.com:443/v1/rankings/character/Kfcbaer/sargeras/US?metric=dps&api_key=879f290422d74824299a945f46eefca3";  
+            var logs;
+
+            tarServer = "sargeras", tarRegion = "US", tarCharacter = "kfcbaer", tarMetric = "dps", pctSum = 0;
 
             request.get({
                 url: apiURL,
@@ -64,22 +60,55 @@ bot.on("message", function(user, userID, channelID, message, event) { // Message
                     }
                     else
                     {
-                        logs = data;
-                        console.log("Retrieved data!");
-                        parseData(logs);
-                        bot.sendMessage({to: channelID, message: returnString});
+                        console.log(typeof data);
+                        console.log(data.constructor.toString().indexOf("Array") > -1);             
+                        console.log(data ? data.length : 'data is null or undefined');   
+                        parseData(data, channelID);
                     }
             });
             break;
+        case "us":
+        case "eu":
+        case "kr":
+        case "cn":
+        case "tw":
+            tarRegion = command[0], tarServer = command[1], tarCharacter = command[2], tarMetric = command[3], pctSum = 0;
+            var apiURL = "https://www.warcraftlogs.com:443/v1/rankings/character/" + tarCharacter + "/" + tarServer + "/" + tarRegion + "?metric=" + tarMetric + "&api_key=" + logsKey;
+            console.log(command[0]);
             
+            request.get({
+                url: apiURL,
+                json: true,
+                headers: {"User-Agent": "request"} },
+                (err, res, data) => {
+                    if (err)
+                    {
+                        console.log("Error:", err);
+                    }
+                    else if (res.statusCode !== 200)
+                    {
+                        console.log("Status:", res.statusCode);
+                    }
+                    else
+                    {
+                        console.log(typeof data);
+                        console.log(data.constructor.toString().indexOf("Array") > -1);             
+                        console.log(data ? data.length : 'data is null or undefined');   
+                        parseData(data, channelID);
+                    }
+            });
+            break;
         }
     }
 });
 
+function parseData(userRankings, channelID) {
 
-function parseData(userRankings) {
-    console.log("Found " + userRankings.length + " reports for "+ tarCharacter + ".");
-    
+/*  console.log("Found " + userRankings.length + " reports for "+ tarCharacter + ".");
+    console.log(typeof userRankings);
+    console.log(userRankings.constructor.toString().indexOf("Array") > -1);
+    console.log(userRankings ? userRankings.length : 'logs is null or undefined');
+*/
     for(var i = 0; i < userRankings.length; i++) {
 
         var userRanking = userRankings[i];
@@ -87,23 +116,19 @@ function parseData(userRankings) {
         pctRank = (userRanking.rank / userRanking.outOf) * 100;
         pctSum += pctRank;
         
-        console.log(pctRank);
+        console.log(pctSum);
         console.log(date);
         console.log("Ranking: " + userRanking.rank + " out of " + userRanking.outOf + ". Percentile ranking: " + pctRank.toFixed());
         
     }
 
     avgRank = (pctSum / userRankings.length).toFixed();
-    returnString = (tarCharacter + " has an average percentile rank of " + avgRank + " for " + tarMetric + ".");
-    console.log(tarCharacter + " has an average percentile rank of " + avgRank + " for " + tarMetric + ".");
+    returnString = ("Character \"" + tarCharacter + "\" has an average rank of " + avgRank + " out of 100 for " + tarMetric.toUpperCase() + ".\n" + userRankings.length + " reports analyzed.");
+    bot.sendMessage({to: channelID, message: returnString});
+    console.log(tarCharacter + " has an average rank of " + avgRank + " out of 100 for " + tarMetric + ".");
 
- /* <!--   bot.sendMessage({to: channelID, message: returnString});     
-
-    document.getElementById("avgRanking").innerHTML = returnString;   */
-    
 }
 
-
-bot.on("disconnect", function() { // Occasionally the bot disconnects.
-    bot.connect(); // Just reconnect when that happens.
+bot.on("disconnect", function() { 
+    bot.connect(); // Attempt to reconnect
 });
